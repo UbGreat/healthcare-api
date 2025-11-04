1c77d9add96af8471c191fa12279d245e2ff525b
UbGreat <ubohodickson@gmail.com>
2025-11-04 22:45:07 +0100

    modified cors

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
     // ✅ Enable CORS
-  app.enableCors({
-    origin: ['http://localhost:4000', 'https://your-frontend-domain.com'], // Add allowed origins
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: false, // If you’re using cookies or authorization headers
-  });
+ // Temporarily enable CORS for all origins
+  app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
 }
 // eslint-disable-next-line prettier/prettier
